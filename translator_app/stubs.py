1614dc20dc6d37446fe6084fe816436d53659013
bogdanduminyuk <77934329bogdan@gmail.com>
2018-09-18 23:16:41 +0300

    javascripts handling of loading page is created

---
+++ translator_app/stubs.py
@@ -0,0 +1,11 @@
+
+
+def generate_input_data():
+    words = ["I", "love", "you", "baby", "admin", "random", 
+             "empty", "custom", "day", "somewhere", "any",
+             "data", "python", "nothing", "progress", 
+             "template", "render", "disable", "put", "local"]
+    return {
+        "words": words,
+        "total": len(words)
+    }
--- translator_app/views.py
+++ translator_app/views.py
@@ -1,29 +1,46 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import render, redirect
 from translator_app.forms import InputForm
+from django.http import JsonResponse
+from .stubs import generate_input_data
 
 
 def index(request):
+    form = InputForm()
+
     if request.method == "POST":
-        # request.POST.dict()
-
         form = InputForm(request.POST)
+
+        data = generate_input_data()
+
         if form.is_valid():
-            print("valid")
-        else:
-            print("not valid")
-
-        # return HttpResponse
-
-    else:
-        form = InputForm()
+            return render(request, 'translator_app/loading.html', {
+                    "total": data["total"],
+                    "current_word": data["words"][0],
+                })
 
     return render(request, 'translator_app/index.html', {'form': form})
 
 
 def loading(request):
-    return render(request, 'translator_app/loading.html', {})
+    if request.method == "GET":
+        data = generate_input_data()
+        idx = int(request.GET['idx'])
+        response = {}
+
+        if idx < data['total']:
+            word = data["words"][idx]
+
+            response["idx"] = idx + 1
+            response["word"] = word
+            response["total"] = data["total"]
+            return JsonResponse(response)
+        else:
+            response["url"] = "/result"
+            response["complete"] = True
+            return JsonResponse(response)
+
+    else:
+        return render(request, 'translator_app/loading.html', {})
 
 
 def result(request):
